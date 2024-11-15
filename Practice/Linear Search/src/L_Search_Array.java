439008ff38883d0859ec1b1ad820baf0426aa0c3
harshavardhan2951 <harshavardhan200101@gmail.com>
2024-11-16 00:43:28 +0530

    Added Linear Search

---
+++ Practice/Linear Search/src/L_Search_Array.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class L_Search_Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> Numb = new ArrayList<>();
+        Numb.add(14);
+        Numb.add(47);
+        Numb.add(49);
+        Numb.add(44);
+        System.out.print("Enter the Target Number: ");
+        int target = sc.nextInt();
+
+        for(int x : Numb) {
+            System.out.println("The available Numbers are: " + x);
+        }
+        if(Numb.contains(target)) {
+            System.out.println("The Target Number " + target + " is available at index: " + Numb.indexOf(target));
+        }else {
+            System.out.println("Nope Not Available...");
+        }
+    }
+}
