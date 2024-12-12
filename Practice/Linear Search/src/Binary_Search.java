6631bad0df0b5ccb58e93778bbfff22604e71767
harshavardhan2951 <harshavardhan200101@gmail.com>
2024-12-12 23:42:14 +0530

    Removed Warnings

--- Practice/Linear Search/src/Binary_Search.java
+++ Practice/Linear Search/src/Binary_Search.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 class Binary_Search {
+    static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
         int[] arr = {2,4,6,9,11,14,20,36,48};
-        int result = findIndex(arr, 36);
+        System.out.print("Enter the Target Element to find: ");
+        int target = input.nextInt();
+        int result = findIndex(arr, target);
         System.out.println(result);
     }
 
@@ -12,14 +17,13 @@ class Binary_Search {
         while(startIndex <= lastIndex) {
             int midElement = startIndex + (lastIndex - startIndex) / 2;
             if(target < Numb[midElement]) {
-                System.out.println("Target is available in the Left Side of the Array....");
                 lastIndex = midElement - 1;
             }
             else if(target > Numb[midElement]) {
                 startIndex = midElement + 1;
-                System.out.println("Target is found in the Right side of the Array....");
             }
             else {
+                System.out.print("Therefore the target Element found in the index of: ");
                 return midElement;
             }
         }
