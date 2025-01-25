f534ac209822267cea5bf73645687fc2c5efec89
Harshavardhan2951 <harshavardhan200101@gmail.com>
2025-01-25 18:23:03 +0530

    Fixed the Issues

--- Practice/BinarySearch_2D/src/BinarySearch_2D.java
+++ Practice/BinarySearch_2D/src/BinarySearch_2D.java
@@ -8,7 +8,8 @@ public class BinarySearch_2D {
                 {10, 20, 30, 40},
                 {15, 25, 35, 45},
                 {28, 29, 39, 48},
-                {32, 33, 42, 53}
+                {32, 33, 42, 53},
+                {61, 62, 63, 64}
         };
         int target = input.nextInt();
         System.out.println(Arrays.toString(findTargetElement(Arr, target)));
@@ -29,44 +30,45 @@ public class BinarySearch_2D {
 
     static int[] findTargetElement(int[][] Numb, int target) {
         int rows = Numb.length;
-        int cols = Numb[0].length;// Be aware the matrix may be empty
-        int rowStart = 0, rowEnd = rows - 1, cmid = cols / 2;
+        int cols = Numb[0].length;//because if there is change in the matrix length
+        int rowStart = 0, rowEnd = rows - 1, cmid = (cols - 1) / 2;
 
-        //Run the Loop till you get the 2 rows
+        //This part of Code applies within the o(log(n)) for the specific midRow and reduces the Array to two sized Array
         while(rowStart < (rowEnd - 1)) {
             int midIndex = rowStart + (rowEnd - rowStart) / 2;
-            if(target == Numb[midIndex][cmid]) {
+            if(Numb[midIndex][cmid] == target) {
                 return new int[]{midIndex, cmid};
-            }if(target > Numb[midIndex][cmid]) {
-                rowStart = midIndex;
-            }else {
-                rowEnd = midIndex;
             }
-        }
-        //Since we had the last rows and their binary search approach successfully
-        //check whether the target is in the column of the two rows
-        if(Numb[rowStart][cmid] == target) {
-            return new int[]{rowStart, cmid};
-        } if(Numb[rowStart + 1][cmid] == target) {
-            return new int[]{rowStart + 1, cmid};
-        }else {
-            rowStart++;
-        }
+            if(Numb[midIndex][cmid] > target) {
+                rowEnd = midIndex - 1;
+            }
+            else {
+                rowStart = midIndex + 1;
+            }
+        }//Since commited to the two rows now apply binary search and find the target in the Index by using the available parts
+        if(Numb[rowStart][cmid] == target) return new int[]{rowStart, cmid};
+
+        if(rowStart + 1 < rows && Numb[rowStart + 1][cmid] == target) return new int[]{rowStart + 1, cmid};
+
+        //Apply Number of possible parts to get the target value directly
         //First Part
-        if(target <= Numb[rowStart][cmid - 1]) {
+        if (target <= Numb[rowStart][cmid - 1]) {
             return BinarySearch(Numb, rowStart, 0, cmid - 1, target);
         }
-        //Second Part
-        if(target >= Numb[rowStart][cmid + 1] && target <= Numb[rowStart][cols - 1]) {
+        // Second Half
+        if (target >= Numb[rowStart][cmid + 1] && target <= Numb[rowStart][cols - 1]) {
             return BinarySearch(Numb, rowStart, cmid + 1, cols - 1, target);
         }
-        //Third Part
-        if(target <= Numb[rowStart + 1][cmid - 1]) {
+        // Third Half
+        if (rowStart + 1 < rows && target <= Numb[rowStart + 1][cmid - 1]) {
             return BinarySearch(Numb, rowStart + 1, 0, cmid - 1, target);
         }
-        //Fourth Part
-        else {
+        // Fourth Half
+        if (rowStart + 1 < rows) {
             return BinarySearch(Numb, rowStart + 1, cmid + 1, cols - 1, target);
         }
+
+        return new int[]{-1, -1}; // Target not found
+
     }
 }
