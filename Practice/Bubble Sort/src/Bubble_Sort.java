056fbe24623b8296d446074c622a30b5b295609f
Harshavardhan2951 <harshavardhan200101@gmail.com>
2025-01-26 23:04:40 +0530

    Bubble Sort Array

---
+++ Practice/Bubble Sort/src/Bubble_Sort.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Bubble_Sort {
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 5, 4, 2};
+        ApplyBubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void ApplyBubbleSort(int[] Numb) {
+        //Run the steps N-1 Times
+        for (int i = 0; i < Numb.length; i++) {
+            //For each step max item will swap to the least element index
+            for (int j = 1; j < Numb.length - i; j++) {
+                //Swap if the number is less than the previous item
+                if(Numb[j] < Numb[j - 1]) {
+                    //swap
+                    int temp = Numb[j];
+                    Numb[j] = Numb[j - 1];
+                    Numb[j - 1] = temp;
+                }
+            }
+        }
+    }
+}
