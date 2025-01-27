ffdf14c786416bec4f93b90a9ea326906a8c6197
Harshavardhan2951 <harshavardhan200101@gmail.com>
2025-01-27 17:41:08 +0530

    Added O(log(n)) cmpxty

--- Practice/LeetCode/src/sqrt_x.java
+++ Practice/LeetCode/src/sqrt_x.java
@@ -1,8 +1,13 @@
 public class sqrt_x {
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
         int[] arr = {2, 3, 4, 5, 6, 7, 8, 9, 10};
         int target = 8;
         System.out.println(sqrt(arr, target));
+        System.out.println("Binary Search: " + findIntSqrt(arr, target));
+        long endTime = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+        System.out.println("Run Time Complexity: " + totalTime + "ms");
     }
 
     //Applied O(N) time complexity
@@ -14,5 +19,20 @@ public class sqrt_x {
                 return x - 1;
             }
         }return -1;
+    }
+
+    //Added Binary Search O(log(n))
+    static int findIntSqrt(int[] arr, int x) {
+        int startIndex = 1, endIndex = arr.length - 1;
+        while(startIndex <= endIndex) {
+            int midIndex = startIndex + (endIndex - startIndex) / 2;
+            if(midIndex > x/midIndex) {
+                endIndex = midIndex - 1;
+            } else if (midIndex < x/midIndex) {
+                startIndex = midIndex + 1;
+            }else  {
+                return midIndex;
+            }
+        }return endIndex;
     }
 }
