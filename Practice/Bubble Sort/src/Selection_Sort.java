2d4892a82604242d5ee9a1743ba74ca363334dfc
Harshavardhan2951 <harshavardhan200101@gmail.com>
2025-02-01 21:29:11 +0530

    Minor Changes in the Main Method

--- Practice/Bubble Sort/src/Selection_Sort.java
+++ Practice/Bubble Sort/src/Selection_Sort.java
@@ -1,18 +1,26 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Selection_Sort {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-        int[] arr = {7, 8, 9, 1, 2, 3};
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        int[] arr = new int[num];
+        for (int n = 0; n < num; n++) {
+            arr[n] = input.nextInt();
+        }
         sort_Algorithm(arr);
-        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i<= arr.length - 1; i++) {
+            System.out.print(arr[i] + " ");
+        }
         long endTime = System.currentTimeMillis();
         long totalTime = endTime - startTime;
-        System.out.println(totalTime + "ms.");
+        System.out.println("\n" + totalTime + "ms.");
     }
 
     static void sort_Algorithm(int[] Numb) {
-        for (int i = 0; i < Numb.length; i++) {
+        for (int i = 0; i < Numb.length - 1; i++) {
             for (int j = 1; j <= Numb.length - i - 1; j++) {
                 swap(Numb, j);
             }
