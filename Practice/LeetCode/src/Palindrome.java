1b7271a72a881c6e7d094a58881be3ad9bb55562
Harshavardhan2951 <harshavardhan200101@gmail.com>
2025-02-08 19:14:34 +0530

    LC 125

---
+++ Practice/LeetCode/src/Palindrome.java
@@ -0,0 +1,21 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        String name = "A man, a plan, a canal: Panama";
+
+        System.out.println(Palindromes(name));
+    }
+
+    static boolean Palindromes(String str) {
+        str = str.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int start = 0, end = str.length() - 1;
+        while(start <= end/2) {
+            if(str.charAt(start) != str.charAt(end)) {
+                return false;
+            }else {
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
+}
