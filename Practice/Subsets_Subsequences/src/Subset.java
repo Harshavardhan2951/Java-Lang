6efef08a6a84257b5d95145c22c06561bb29500b
Harshavardhan2951 <harshavardhan200101@gmail.com>
2025-03-11 00:14:04 +0530

    Added SubSet Recursive

---
+++ Practice/Subsets_Subsequences/src/Subset.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class Subset {
+    public static void main(String[] args) {
+        String name = "abc";
+        SubSeq_Approach("", name);
+        System.out.println(subSequence("", name));
+    }
+
+    static void SubSeq_Approach(String left, String right) {
+        //Base Condition
+        if(right.isEmpty()) {
+            System.out.println("[" + left + "]");
+            return;
+        }
+        //Logic
+        char ch = right.charAt(0);
+        SubSeq_Approach(left + ch, right.substring(1));
+        SubSeq_Approach(left, right.substring(1));
+    }
+
+    static ArrayList<String> subSequence(String p, String up) {
+        //Base Condition
+        if(up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        //Logic
+        char ch = up.charAt(0);
+        ArrayList<String> left = subSequence(p + ch, up.substring(1));
+        ArrayList<String> right = subSequence(p, up.substring(1));
+        left.addAll(right);
+        return left;
+    }
+}
